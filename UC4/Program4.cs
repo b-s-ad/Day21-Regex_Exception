3eaf036339e9eed76d0ba55ae3ef6a49973f6b08
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-12 23:20:11 -0700

    UC 4 : Pre-defined Mobile Format

---
+++ UC4/Program4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace day21
+{
+    class Program4
+    {
+        public static void Main(string[] args)
+        {
+            
+            UC4P pattern = new UC4P(); // Regex Pattern
+            
+            Write("\tUC 4 : Pre-defined Mobile Format \n");
+             
+            WriteLine(pattern.UC_4("91 9919819801"));
+            
+            ReadLine();
+            
+        }
+    }
+}
---
+++ UC4/UC4P.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace day21
+{
+    public class UC4P
+    {
+        public static string Regex_input = "^[9][1][ ][1-9][0-9]{9}$";
+        public bool UC_4(string input)
+        {
+
+            
+                if (input == null)
+                {
+                    throw new URegCustomException(URegCustomException.ExceptionType.INVALID_MESSAGE, "Invalid Input");
+                }
+                return System.Text.RegularExpressions.Regex.IsMatch(input, Regex_input);
+            }
+    }
+    
+}
