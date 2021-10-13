1c893ca8bb443d7fba65a1f80e03d6badb89d0c9
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-12 23:37:48 -0700

    UC 8 : At least 1 Special Char

---
+++ UC8/Program8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace day21
+{
+    class Program8
+    {
+        public static void Main(string[] args)
+        {
+
+            UC8P pattern = new UC8P(); // Regex Pattern
+
+            Write("\tUC6 Should have at least 1 Special Character\n");
+
+            WriteLine(pattern.UC_8("Abc@123D"));
+            
+            ReadLine();
+            
+        }
+    }
+}
---
+++ UC8/UC8P.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+
+namespace day21
+{
+    public class UC8P
+    {
+        public static string Regex_input = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&^_-]{8,}$";
+        public bool UC_8(string input)
+        {
+
+            
+                if (input == null)
+                {
+                    throw new URegCustomException(URegCustomException.ExceptionType.INVALID_MESSAGE, "Invalid Input");
+                }
+                return System.Text.RegularExpressions.Regex.IsMatch(input, Regex_input);
+            }
+    }
+    
+}
