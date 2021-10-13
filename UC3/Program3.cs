0a93a330d38e4d924f3dc2e61706d6265fbc53cf
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-12 23:16:25 -0700

    UC : 3 Valid Email 3 mandatory | 2 optional parts

---
+++ UC3/Program3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace day21
+{
+    class Program3
+    {
+        public static void Main(string[] args)
+        {
+            
+            UC3P pattern = new UC3P(); // Regex Pattern
+            
+            Write("\tUC : 3 Valid Email 3 mandatory | 2 optional parts \n");
+             
+            WriteLine(pattern.UC_3("abc.xyz@bl.co.in"));
+            
+            ReadLine();
+            
+        }
+    }
+}
---
+++ UC3/UC3P.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace day21
+{
+    public class UC3P
+    {
+        public static string Regex_UC3 = "^[a-z][-a-z0-9._]+@([-a-z0-9{2,2}]+[.])+[a-z]{2,2}$";
+        public bool UC_3(string input)
+        {
+
+            
+                if (input == null)
+                {
+                    throw new URegCustomException(URegCustomException.ExceptionType.INVALID_MESSAGE, "Invalid Input");
+                }
+                return System.Text.RegularExpressions.Regex.IsMatch(input, Regex_UC3);
+            }
+    }
+    
+}
