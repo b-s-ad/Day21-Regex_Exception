3415f4533e9ae2dd0d01128b57236719614d43d2
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-12 23:35:00 -0700

    UC 7: Have at least 1 Numeric Number

---
+++ UC7/Program7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace day21
+{
+    class Program7
+    {
+        public static void Main(string[] args)
+        {
+            
+            UC7P pattern = new UC7P(); // Regex Pattern
+
+            Write("\tUC6 Should have at least 1 Numeric Number in the password\n");
+
+            WriteLine(pattern.UC_7("1Abcde@#"));
+            
+            ReadLine();
+            
+        }
+    }
+}
---
+++ UC7/UC7P.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace day21
+{
+    public class UC7P
+    {
+        public static string Regex_input = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&^_-]{8,}$";
+        public bool UC_7(string input)
+        {
+
+            
+                if (input == null)
+                {
+                    throw new URegCustomException(URegCustomException.ExceptionType.INVALID_MESSAGE, "Invalid Input");
+                }
+                return System.Text.RegularExpressions.Regex.IsMatch(input, Regex_input);
+            }
+    }
+    
+}
