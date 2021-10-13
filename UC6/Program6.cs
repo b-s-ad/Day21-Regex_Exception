fed506b5fa739f041f384467a68ae8aa3c5e12d1
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-12 23:31:27 -0700

    UC 6 : Have at least 1 Upper Case

---
+++ UC6/Program6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace day21
+{
+    class Program6
+    {
+        public static void Main(string[] args)
+        {
+            
+            UC6P pattern = new UC6P(); // Regex Pattern
+            
+            Write("\tUC6 Should have at least 1 Upper Case\n");
+             
+            WriteLine(pattern.UC_6("abcdA12@$"));
+            
+            ReadLine();
+            
+        }
+    }
+}
