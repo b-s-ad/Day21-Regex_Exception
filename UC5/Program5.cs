09ef561f848a5d521fe25a8f528250dd44857c5c
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-12 23:27:39 -0700

    UC 5 : Minimum 8 Char

---
+++ UC5/Program5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace day21
+{
+    class Program5
+    {
+        public static void Main(string[] args)
+        {
+            
+            UC5P pattern = new UC5P(); // Regex Pattern
+            
+            Write("\tUC 5 : Minimum 8 Char \n");
+             
+            WriteLine(pattern.UC_5("AbCD12#$"));
+            
+            ReadLine();
+            
+        }
+    }
+}
